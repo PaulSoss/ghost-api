301ea396d575daa7bd6fc09d26166e15af9945ca
PaulSoss <merghemsofyane@gmail.com>
2026-03-19 22:25:50 +0100

    Update api.py

--- api.py
+++ api.py
@@ -125,3 +125,7 @@ def stats():
         "total_xp": row["total_xp"],
         "niveau_max": row["max_level"],
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
